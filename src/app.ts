7317e135aa76239d6cb2e45c9da02193cedb14cd
Michael Braga <michael_braga@outlook.com>
2020-06-27 02:01:05 -0400

    Basic pug template setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import {ApolloServer} from 'apollo-server-express';
+import {join} from 'path';
 import env from './env.config';
 import database from './database';
+import {home} from './routes/';
 
 import typeDefs from './typeDefs';
 import models from './models';
@@ -10,6 +12,8 @@ import resolvers from './resolvers';
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', join(__dirname, 'views'));
 app.use(cors());
 
 const server = new ApolloServer({
@@ -18,6 +22,10 @@ const server = new ApolloServer({
 	context: {Category: models.Category, Post: models.Post}
 });
 server.applyMiddleware({app});
+
+app.get('/', home);
+
+//make error route and default error route
 
 database()
 	.then(() => console.log('mongodb connected'));
